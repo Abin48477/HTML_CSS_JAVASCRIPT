2626f264b41b38acd976482720737818f569cb76
Abin Ghimire <ghimireabin56@gmail.com>
2025-09-02 14:16:53 +0545

    updated

---
+++ LearnJavaScript/myScript.js
@@ -0,0 +1,10 @@
+
+// External scripts are practical when the same code is used in many different web pages.
+// JavaScript files have the file extension .js.
+
+// To use an external script, put the name of the script file in the src (source) attribute of a <script> tag:
+// Example
+//<script src="myScript.js"></script>
+function myfunction() {
+    document.getElementById("mydemo").innerHTML = "Hare Ram Hare Ram";
+}
